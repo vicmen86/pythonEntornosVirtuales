708b5c244563eed5308af0a21ad4c0dcd33222fb
vicmen86 <victrinsumos@gmail.com>
2023-10-03 13:17:58 -0300

    Libreria matplotlib

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Steps
+# Game Project
+Parra correr el juego:
+```sh
+cd game
+python3 main.py
+```
---
+++ charts/main.py
@@ -0,0 +1,7 @@
+import charts
+
+def run():
+  charts.generatePieChart()
+
+if __name__=='__main__':
+  run()
